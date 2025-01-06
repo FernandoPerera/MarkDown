f4c7b699a1ff416fd547b66d3a3d4db834ec4d4f
FernandoPerera <fernando02perera@gmail.com>
2025-01-06 13:49:00 +0000

    :white_check_mark: turn_links_into_footnotes_and_replace_anchors

--- src/main/java/MarkDownTransformer.java
+++ src/main/java/MarkDownTransformer.java
@@ -1,5 +1,44 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public final class MarkDownTransformer {
+
     public void execute(String inputFilePath, String outputFilePath) {
-        throw new RuntimeException("not implemented yet");
+        File inputFile = new File(inputFilePath);
+        File outputFile = new File(outputFilePath);
+
+        String content = "";
+        try (FileReader reader = new FileReader(inputFile)) {
+            StringBuilder stringBuilder = new StringBuilder();
+            int accumulator;
+            while ((accumulator = reader.read()) != -1) {
+                stringBuilder.append((char) accumulator);
+            }
+            content = stringBuilder.toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Matcher visibleTextMatcher = Pattern.compile("\\[(.*?)]").matcher(content);
+        visibleTextMatcher.find();
+        String visibleText = visibleTextMatcher.group(1);
+
+        Matcher urlMatcher = Pattern.compile("\\((.*?)\\)").matcher(content);
+        urlMatcher.find();
+        String urlText = urlMatcher.group(1);
+
+        String contentToInput = visibleText + " [^anchor1]\n[^anchor1]: " + urlText;
+
+        try (FileWriter writer = new FileWriter(outputFile)) {
+            writer.write(contentToInput);
+            writer.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
+
 }
