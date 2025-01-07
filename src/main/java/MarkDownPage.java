47911e098191dbe6791e1b32b5b274328fa37ec6
FernandoPerera <fernando02perera@gmail.com>
2025-01-07 20:56:54 +0000

    :recycle: delegated transformation to MarkDownPage class

---
+++ src/main/java/MarkDownPage.java
@@ -0,0 +1,75 @@
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public final class MarkDownPage {
+
+    private String content;
+
+    private final static String LINKED_TEXT_REGEX = "\\[([^]]+)]\\(([^)]+)\\)";
+
+    public MarkDownPage(String content) {
+        this.content = content;
+    }
+
+    public void buildFooterMovingLinks() {
+        HashMap<String, String> anchors = findLinksInPage();
+
+        removeBrackets();
+        replaceLinksWithAnchors(anchors);
+
+        addLinksToFootNotes(anchors);
+    }
+
+    private void removeBrackets() {
+        Matcher matcher = Pattern.compile(LINKED_TEXT_REGEX).matcher(content);
+
+        while (matcher.find()) {
+            String linkedText = matcher.group(1);
+            String linkedTextWithBrackets = String.format("[%s]", linkedText);
+            content = content.replace(linkedTextWithBrackets, linkedText);
+        }
+    }
+
+    private void addLinksToFootNotes(HashMap<String, String> anchors) {
+        List<Map.Entry<String, String>> list = new ArrayList<>(anchors.entrySet());
+        Collections.reverse(list);
+
+        StringBuilder contentWithFootNotes = new StringBuilder(content);
+        for (Map.Entry<String, String> entry : list) {
+            contentWithFootNotes.append(String.format("\n%s: %s", entry.getKey(), entry.getValue()));
+        }
+        this.content = contentWithFootNotes.toString();
+    }
+
+
+    private void replaceLinksWithAnchors(HashMap<String, String> anchors) {
+        anchors.forEach((anchor, url) -> {
+            String linkWithParenthesis = String.format("(%s)", url);
+            String anchorText = String.format(" %s", anchor);
+            content = content.replace(linkWithParenthesis, anchorText);
+        });
+    }
+
+    private HashMap<String, String> findLinksInPage() {
+        HashMap<String, String> anchors = new HashMap<>();
+        int anchorIndex = 0;
+
+        Matcher matcher = Pattern.compile(LINKED_TEXT_REGEX).matcher(content);
+
+        while (matcher.find()) {
+            anchorIndex++;
+            String url = matcher.group(2);
+            anchors.put(
+                    String.format("[^anchor%s]", anchorIndex),
+                    url
+            );
+        }
+
+        return anchors;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+}
--- src/main/java/MarkDownTransformer.java
+++ src/main/java/MarkDownTransformer.java
@@ -1,13 +1,9 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public final class MarkDownTransformer {
 
     private final FileManager fileManager;
-
-    private final static String LINKED_TEXT_REGEX = "\\[([^]]+)]\\(([^)]+)\\)";
 
     public MarkDownTransformer(FileManager fileManager) {
         this.fileManager = fileManager;
@@ -20,9 +16,11 @@ public final class MarkDownTransformer {
         checkForFiles(inputFile, outputFile);
 
         String inputFileContent = fileManager.read(inputFile);
-        String transformedContent = transformContentWithAnchor(inputFileContent, 1);
 
-        fileManager.writeContent(transformedContent, outputFile);
+        MarkDownPage page = new MarkDownPage(inputFileContent);
+        page.buildFooterMovingLinks();
+
+        fileManager.writeContent(page.getContent(), outputFile);
     }
 
     private void checkForFiles(File inputFile, File outputFile) throws FileNotFoundException {
@@ -32,43 +30,6 @@ public final class MarkDownTransformer {
         if (!fileManager.exists(outputFile)) {
             throw new FileNotFoundException("File not found: " + outputFile.getAbsolutePath());
         }
-    }
-
-    private String transformContentWithAnchor(String content, int anchorNumber) {
-        Matcher linkedTextMatcher = Pattern.compile(LINKED_TEXT_REGEX).matcher(content);
-        boolean needsTransformation = linkedTextMatcher.find();
-
-        if (needsTransformation) {
-            String linkedText = linkedTextMatcher.group(1);
-            String url = linkedTextMatcher.group(2);
-
-            String contentWithoutTransformation = content.substring(content.indexOf(url) + url.length() + 1);
-            String replacedTextByAnchor = String.format("%s [^anchor%s]%s", linkedText, anchorNumber, contentWithoutTransformation);
-            String lineBreak = "\n";
-
-            StringBuffer transformedContent = new StringBuffer()
-                    .append(getInitialContent(content, linkedText))
-                    .append(replacedTextByAnchor)
-                    .append(lineBreak)
-                    .append(buildFootnote(anchorNumber, url));
-
-            Matcher linkedTextInRestMatcher = Pattern.compile(LINKED_TEXT_REGEX).matcher(transformedContent);
-            boolean needsMoreTransformation = linkedTextInRestMatcher.find();
-
-            return needsMoreTransformation
-                    ? transformContentWithAnchor(transformedContent.toString(), anchorNumber + 1)
-                    : transformedContent.toString();
-        }
-        
-        return content;
-    }
-
-    private static String getInitialContent(String content, String linkedText) {
-        return content.substring(0, content.indexOf("[" + linkedText + "]"));
-    }
-
-    private static String buildFootnote(int anchorNumber, String url) {
-        return String.format("[^anchor%s]: %s", anchorNumber, url);
     }
 
 }
